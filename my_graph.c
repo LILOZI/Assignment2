8ddb547705ae75220003b9b85671fdb769edf7f8
Oz Atar <108188108+LILOZI@users.noreply.github.com>
2024-02-12 19:12:44 +0200

    Update my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -5,27 +5,27 @@ int main()
 {
     int graph[MAT_SIZE][MAT_SIZE] = {0};
 
-    char userChoise;
+    char userChoice;
     int i, j;
 
-    scanf("%c", &userChoise);
-    while (userChoise != 'D') // Exit the program
+    scanf("%c", &userChoice);
+    while (userChoice != 'D' && userChoice) // Exit the program
     {   
-        if (userChoise == 'A') // Entering value to the graph that represented as a matrix
+        if (userChoice == 'A') // Entering value to the graph that represented as a matrix
         {
             matValues(graph, MAT_SIZE);
         }
-        else if (userChoise == 'B') // Finding a path from i to j in the graph
+        else if (userChoice == 'B') // Finding a path from i to j in the graph
         {
             scanf("%d%d", &i, &j);
             printf("%s\n", isPath(graph, MAT_SIZE, i, j) ? "True" : "False");
         }
-        else if (userChoise == 'C') // If there is a path fiding the shortest-path
+        else if (userChoice == 'C') // If there is a path fiding the shortest-path
         {
             scanf("%d%d", &i, &j);
             printf("%d\n", shortesPath(graph, MAT_SIZE, i, j));
         }
-        scanf(" %c", &userChoise); // For entering another choise
+        scanf(" %c", &userChoice); // For entering another choise
     }
 
     return 0;
